ff7cff68b66d2ab12b8d7a43291e7e5d626ee576
Unknown <akshat1@mail.usf.edu>
2019-05-13 04:18:00 +0530

    Added location icon and location. Magnitude in the Circular icon.

--- quake_app/lib/main.dart
+++ quake_app/lib/main.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:convert';
-import 'package:http/http.dart' as http ;
+import 'package:http/http.dart' as http;
 
 Map _data;
 
 void main() async {
-
   _data = await getQuakes();
-  print(_data['features'][0]['properties']);
 
   runApp(new MaterialApp(
     title: 'Earthquakes',
@@ -30,14 +28,51 @@ class Quakes extends StatelessWidget {
 
       body: new Center(
         child: new ListView.builder(
-            itemCount: _data['features'].length,
-            padding: const EdgeInsets.all(16.0),
-            itemBuilder: (BuildContext context, int position){
-              if(position.isOdd) return new Divider();
-              return ListTile(
-                title: new Text("${_data['features'][position]['properties']['place']}"),
-              );
-            },
+          itemCount: _data['features'].length,
+          padding: const EdgeInsets.all(16.0),
+          itemBuilder: (BuildContext context, int position) {
+            if (position.isOdd) return new Divider();
+            final index = position ~/ 2;
+            return ListTile(
+              title: new Text(
+                "Magnitude: ${_data['features'][index]['properties']['mag']}",
+                style: new TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.teal,
+                    fontWeight: FontWeight.w500
+                ),
+              ),
+              subtitle: new Container(
+                child: new Row(
+                    children: <Widget>[
+                new Icon(Icons.location_on,
+                color: Colors.tealAccent,),
+                new Expanded(
+                  child: new Text("${_data['features'][index]['properties']['place']}",
+                    style: new TextStyle(
+                        fontSize: 15.0,
+                        color: Colors.teal[300],
+                        fontWeight: FontWeight.w300,
+                        fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                ),
+              ],
+
+            ),),
+              leading: new CircleAvatar(
+                backgroundColor: Colors.tealAccent,
+                child: new Text(
+                    "${_data['features'][index]['properties']['mag']}",
+                      style: new TextStyle(
+                        fontSize: 14.0,
+                        color: Colors.teal[800],
+                        fontWeight: FontWeight.w800
+                      ),
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
@@ -46,9 +81,9 @@ class Quakes extends StatelessWidget {
 }
 
 Future<Map> getQuakes() async {
-  String apiUrl ='https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
+  String apiUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
 
-  http.Response response =await http.get(apiUrl);
+  http.Response response = await http.get(apiUrl);
 
   return jsonDecode(response.body);
 }
