620287e18690ace7815cb1b03b0e085a94ba48ab
Unknown <akshat1@mail.usf.edu>
2019-05-13 06:06:34 +0530

    Adding an alert box

--- quake_app/lib/main.dart
+++ quake_app/lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
+
 Map _data;
 
 void main() async {
@@ -35,8 +36,9 @@ class Quakes extends StatelessWidget {
             final index = position ~/ 2;
 
             var format = new DateFormat.yMMMd().add_jm();
-            var date = format.format(new DateTime.fromMicrosecondsSinceEpoch(_data['features'][index]['properties']['time']*1000,
-            isUtc: true));
+            var date = format.format(new DateTime.fromMicrosecondsSinceEpoch(
+                _data['features'][index]['properties']['time'] * 1000,
+                isUtc: true));
             return ListTile(
               title: Container(
                 child: Row(
@@ -44,7 +46,7 @@ class Quakes extends StatelessWidget {
                     Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: new Icon(Icons.access_time,
-                      color: Colors.tealAccent.shade700,),
+                        color: Colors.tealAccent.shade700,),
                     ),
                     new Text(
                       "$date",
@@ -59,41 +61,60 @@ class Quakes extends StatelessWidget {
               ),
               subtitle: new Container(
                 child: new Row(
-                    children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: new Icon(Icons.location_on,
-                  color: Colors.tealAccent.shade700,),
-                ),
-                new Expanded(
-                  child: new Text("${_data['features'][index]['properties']['place']}",
-                    style: new TextStyle(
-                        fontSize: 15.0,
-                        color: Colors.teal[300],
-                        fontWeight: FontWeight.w300,
-                        fontStyle: FontStyle.italic,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: new Icon(Icons.location_on,
+                        color: Colors.tealAccent.shade700,),
                     ),
-                  ),
-                ),
-              ],
+                    new Expanded(
+                      child: new Text(
+                        "${_data['features'][index]['properties']['place']}",
+                        style: new TextStyle(
+                          fontSize: 15.0,
+                          color: Colors.teal[300],
+                          fontWeight: FontWeight.w300,
+                          fontStyle: FontStyle.italic,
+                        ),
+                      ),
+                    ),
+                  ],
 
-            ),),
+                ),),
               leading: new CircleAvatar(
                 backgroundColor: Colors.tealAccent,
                 child: new Text(
-                    "${_data['features'][index]['properties']['mag']}",
-                      style: new TextStyle(
-                        fontSize: 14.0,
-                        color: Colors.teal[800],
-                        fontWeight: FontWeight.w800
-                      ),
+                  "${_data['features'][index]['properties']['mag']}",
+                  style: new TextStyle(
+                      fontSize: 14.0,
+                      color: Colors.teal[800],
+                      fontWeight: FontWeight.w800
+                  ),
                 ),
               ),
+              onTap: () {
+                _showAlertMessage(
+                    context, "${_data['features'][index]['properties']['title']}");
+              },
             );
           },
         ),
       ),
     );
+  }
+
+  void _showAlertMessage(BuildContext context, String message) {
+    var alert = new AlertDialog(
+      title: new Text('Earthquake'),
+      content: new Text(message),
+      actions: <Widget>[
+        new FlatButton(onPressed: () {
+          Navigator.pop(context);
+        },
+            child: new Text('OK'))
+      ],
+    );
+    showDialog(context: context, child: alert);
   }
 
 }
